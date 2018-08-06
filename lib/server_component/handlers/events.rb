584a9581fd1e8c80376ed21cf135c522baa0489b
Julian Popescu <jpopesculian@gmail.com>
2018-08-06 15:09:54 +0200

    indempotent events

--- lib/server_component/handlers/events.rb
+++ lib/server_component/handlers/events.rb
@@ -9,25 +9,34 @@ module ServerComponent
 
       dependency :clock, Clock::UTC
       dependency :sms_store, Sms::Store
-      dependency :message_fetched_trigger, Triggers::MessageFetchedTrigger
+
       dependency :finished, Utils::RequestCallbacks::Finished
       dependency :processed, Utils::Processed
+
+      dependency :message_fetched_trigger, Triggers::MessageFetchedTrigger
+
+      dependency :text_receive, Models::Mutations::TextReceive
+      dependency :text_send, Models::Mutations::TextSend
 
       def configure
         Clock::UTC.configure(self)
         Sms::Store.configure(self, attr_name: :sms_store)
-        Triggers::MessageFetchedTrigger.configure(self)
+
         Utils::RequestCallbacks::Finished.configure(self)
         Utils::Processed.configure(self)
+
+        Triggers::MessageFetchedTrigger.configure(self)
+
+        Models::Mutations::TextReceive.configure(self)
+        Models::Mutations::TextSend.configure(self)
       end
 
       category :server
 
       handle SmsReceived do |sms_received|
         sms = sms_store.get(sms_received.sms_id)
-        # TODO convert model functions into dependency objects
-        # text = Models::Text::Receive.(sms)
-        # message_fetched_trigger.(text)
+        text = text_receive.(sms)
+        message_fetched_trigger.(text)
 
         current, ignored = processed.(sms_received)
         return ignored.() if current
@@ -36,8 +45,8 @@ module ServerComponent
       end
 
       handle SmsSent do |sms_sent|
-        # sms = sms_store.get(sms_sent.sms_id)
-        # Models::Text::Send.(sms)
+        sms = sms_store.get(sms_sent.sms_id)
+        text = text_send.(sms)
 
         current, ignored = processed.(sms_sent)
         return ignored.() if current
